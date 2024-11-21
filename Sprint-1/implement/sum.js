7bed97c4cd16c346854c61b3374578345add3837
azaralampanah2 <azaralampanah2@gmail.com>
2024-11-21 06:50:43 +0000

    complete Sprint-1/implement/sum.js& sum.test.js

--- Sprint-1/implement/sum.js
+++ Sprint-1/implement/sum.js
@@ -1,4 +1,23 @@
 function sum(elements) {
+  let sum1 = 0;
+  if (elements.every((Element) => typeof Element != "number")) {
+    //if all the elemnts in the array are non-numerical it will add up the ASCII value of all characters
+
+    for (let element of elements) {
+      for (i = 0; i < element.length; i++) {
+        sum1 += element.charCodeAt(i);
+      }
+    }
+    return sum1;
+  }
+
+  for (let element of elements) {
+    if (typeof element === "number") {
+      sum1 += element;
+    }
+  }
+  return sum1;
 }
 
+//console.log(sum([1,2,3,"bhk"]));
 module.exports = sum;
--- Sprint-1/implement/sum.test.js
+++ Sprint-1/implement/sum.test.js
@@ -13,23 +13,37 @@ const sum = require("./sum.js");
 // Given an empty array
 // When passed to the sum function
 // Then it should return 0
-test.todo("given an empty array, returns 0")
+test("for an empty array should return 0", ()=>{
+    expect(sum([])).toBe(0)
+})
 
 // Given an array with just one number
 // When passed to the sum function
 // Then it should return that number
+test("for an array with one number should return that number",()=>{
+  expect(sum([4])).toBe(4)  
+})
 
 // Given an array containing negative numbers
 // When passed to the sum function
 // Then it should still return the correct total sum
+test("for an array with negative numbers should return sum of them",()=>{
+    expect(sum([-5,-9,-4,-10])).toBe(-28)
+})
 
 // Given an array with decimal/float numbers
 // When passed to the sum function
 // Then it should return the correct total sum
+test("for an array with decimal numbers should return sum of them",()=>{
+    expect(sum([5.2,1.9,8.3])).toEqual(15.4)
+})
 
 // Given an array containing non-number values
 // When passed to the sum function
 // Then it should ignore the non-numerical values and return the sum of the numerical elements
+test("if the array has non-numerical values inside, it should ignore them and just add up the numbers", ()=>{
+    expect(sum(["hi",5,4,3,"NO"])).toBe(12)
+})
 
 // Given an array with only non-number values
 // When passed to the sum function
