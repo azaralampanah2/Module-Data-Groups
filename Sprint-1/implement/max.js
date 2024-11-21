f25d112bcd8b195f2686d29ec88eec25f5a2fb85
azaralampanah2 <azaralampanah2@gmail.com>
2024-11-21 05:24:16 +0000

    complete Sprint-1/implement/test.js

--- Sprint-1/implement/max.js
+++ Sprint-1/implement/max.js
@@ -1,4 +1,19 @@
 function findMax(elements) {
-}
+  let maxNumber = elements[0];
+  if (elements.length === 0) {
+    return "-Infinity";
+  }
+  
 
+  for (let i = 1; i < elements.length; i++) {
+    if (maxNumber < elements[i]) {
+      maxNumber = elements[i];
+    }
+  }
+  return maxNumber;
+
+
+ 
+}
+console.log(findMax([]));
 module.exports = findMax;
