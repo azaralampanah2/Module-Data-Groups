363d3d4db3cc3c32e09a703e3035c4347c8b838c
azaralampanah2 <azaralampanah2@gmail.com>
2024-11-25 13:07:36 +0000

    complete sprint-2/implement/tally.js & tally.test.js

--- Sprint-2/implement/tally.js
+++ Sprint-2/implement/tally.js
@@ -1,3 +1,14 @@
-function tally() {}
-
+function tally(mainArray) {
+  const frequencyObject = {};
+  if (typeof mainArray != "object") {
+    return "Error! Input must be an array";
+  }
+  for (element of mainArray) {
+    if (frequencyObject[element]) {
+      frequencyObject[element] += 1;
+    } else frequencyObject[element] = 1;
+  }
+  return frequencyObject;
+}
+console.log(tally("aghdgdg"));
 module.exports = tally;
--- Sprint-2/implement/tally.test.js
+++ Sprint-2/implement/tally.test.js
@@ -19,11 +19,24 @@ const tally = require("./tally.js");
 // Given a function called tally
 // When passed an array of items
 // Then it should return an object containing the count for each unique item
+test("should return the frequency of each element of the array in an object", () => {
+  expect(tally(["a", "a", "c", "b", "c", "b", "b"])).toStrictEqual({
+    a: 2,
+    b: 3,
+    c: 2,
+  });
+  expect(tally(["a"])).toStrictEqual({ a: 1 });
+  expect(tally(["a", "a", "a"])).toStrictEqual({ a: 3 });
+  expect(tally(["a", "a", "b", "c"])).toStrictEqual({ a: 2, b: 1, c: 1 });
+});
 
 // Given an empty array
 // When passed to tally
 // Then it should return an empty object
-test.todo("tally on an empty array returns an empty object");
+//test.todo("tally on an empty array returns an empty object");
+test("should return empty object with empty array", () => {
+  expect(tally([])).toStrictEqual({});
+});
 
 // Given an array with duplicate items
 // When passed to tally
@@ -32,3 +45,7 @@ test.todo("tally on an empty array returns an empty object");
 // Given an invalid input like a string
 // When passed to tally
 // Then it should throw an error
+test("return error, as the input is not an array", () => {
+  expect(tally(4353636)).toBe("Error! Input must be an array");
+  expect(tally("Hello ")).toBe("Error! Input must be an array");
+});
