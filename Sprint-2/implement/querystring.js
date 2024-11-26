3ce0e6fcf5f9fdd00ebcea86e316ece6eacf2cb5
azaralampanah2 <azaralampanah2@gmail.com>
2024-11-26 06:22:02 +0000

    complete sprint-2/implement/querystring.js&querystring.test.js

--- Sprint-2/implement/querystring.js
+++ Sprint-2/implement/querystring.js
@@ -4,13 +4,18 @@ function parseQueryString(queryString) {
     return queryParams;
   }
   const keyValuePairs = queryString.split("&");
-
+  console.log(keyValuePairs);
   for (const pair of keyValuePairs) {
-    const [key, value] = pair.split("=");
+    const index1 = pair.indexOf("=");
+    console.log(index1);
+    const key = pair.slice(0, index1);
+    const value = pair.slice(index1 + 1);
+    console.log(value);
+    console.log(key);
     queryParams[key] = value;
   }
 
   return queryParams;
 }
-
+//console.log(parseQueryString(" "));
 module.exports = parseQueryString;
--- Sprint-2/implement/querystring.test.js
+++ Sprint-2/implement/querystring.test.js
@@ -3,10 +3,25 @@
 // Below is one test case for an edge case the implementation doesn't handle well.
 // Fix the implementation for this test, and try to think of as many other edge cases as possible - write tests and fix those too.
 
-const parseQueryString = require("./querystring.js")
+const parseQueryString = require("./querystring.js");
 
 test("parses querystring values containing =", () => {
-  expect(parseQueryString("equation=x=y+1")).toEqual({
-    "equation": "x=y+1",
+  expect(parseQueryString("equation=x=y+1")).toStrictEqual({
+    equation: "x=y+1",
+  });
+});
+test("parses querystring ", () => {
+  expect(parseQueryString("string1=x+1&string2=x-1")).toStrictEqual({
+    string1: "x+1",
+    string2: "x-1",
+  });
+});
+test("an empty querystring should return {} ", () => {
+  expect(parseQueryString("")).toStrictEqual({});
+});
+test("parses querystring with empty value", () => {
+  expect(parseQueryString("string1=x+1&string2=")).toStrictEqual({
+    string1: "x+1",
+    string2: "",
   });
 });
