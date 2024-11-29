7fa1899662b034c20aee3156e864ff0ba12e62c6
azaralampanah2 <azaralampanah2@gmail.com>
2024-11-29 05:49:32 +0000

    optimized Sprint-3/reading-list

--- Sprint-3/reading-list/script.js
+++ Sprint-3/reading-list/script.js
@@ -20,47 +20,33 @@ const books = [
     bookCoverImage: "https://blackwells.co.uk/jacket/l/9780135957059.jpg",
   },
 ];
-const firstBook = document.getElementById("firstBookDiv");
-const secondBook = document.getElementById("secondBookDiv");
-const thirdBook = document.getElementById("thirdBookDiv");
 
-firstBook.textContent = books[0].title + " by " + books[0].author;
-const firstBookImage = document.createElement("img");
-firstBookImage.src = books[0].bookCoverImage;
-firstBookImage.alt = "The design of Everyday Things Book Image";
-firstBook.appendChild(firstBookImage);
-firstBookImage.width = 50;
-firstBookImage.height = 50;
+for (i = 0; i < books.length; i++) {
+    let container = document.getElementById("reading-list")
 
-secondBook.textContent = books[1].title + " by " + books[1].author;
-const secondBookImage = document.createElement("img");
-secondBookImage.src = books[1].bookCoverImage;
-secondBookImage.alt = "The Most Human Human  Book Image";
-secondBook.appendChild(secondBookImage);
-secondBookImage.width = 50;
-secondBookImage.height = 50;
+    let listItem = document.createElement("li");
+    let text = document.createElement("p")
+    let image = document.createElement("img")
 
-thirdBook.textContent = books[2].title + " by " + books[2].author;
-const thirdBookImage = document.createElement("img");
-thirdBookImage.src = books[2].bookCoverImage;
-secondBookImage.alt = "The Pragmatic Programmer Book Image";
-thirdBook.appendChild(thirdBookImage);
-thirdBookImage.width = 50;
-thirdBookImage.height = 50;
-if (books[0].alreadyRead == true) {
-  firstBook.style.backgroundColor = "green";
-} else {
-  firstBook.style.backgroundColor = "red";
+    let title = books[i].title + " by " + books[i].author
+    text.innerHTML = title
+    text.style.fontSize = 18
+
+    image.src = books[i].bookCoverImage
+    image.width = 125
+    image.alt = books[i].title
+
+    listItem.style.marginBottom = 15
+
+    books[i].alreadyRead ? listItem.style.backgroundColor = "green" : listItem.style.backgroundColor = "red"
+
+    listItem.appendChild(text)
+    listItem.appendChild(image)
+
+    container.appendChild(listItem)
 }
-if (books[1].alreadyRead == true) {
-  secondBook.style.backgroundColor = "green";
-} else {
-  firstBook.style.backgroundColor = "red";
-}
-if (books[2].alreadyRead == true) {
-  thirdBook.style.backgroundColor = "green";
-} else {
-  thirdBook.style.backgroundColor = "red";
-}
+
+
+
 
 
