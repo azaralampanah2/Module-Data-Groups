69a5f4f37de55175cba933e05673fcc7da9888bc
azaralampanah2 <azaralampanah2@gmail.com>
2024-12-02 12:29:17 +0000

    complete Sprint-3/slidshow

--- Sprint-3/slideshow/slideshow.js
+++ Sprint-3/slideshow/slideshow.js
@@ -1,8 +1,50 @@
 const images = [
-    "./assets/cute-cat-a.png",
-    "./assets/cute-cat-b.jpg",
-    "./assets/cute-cat-c.jpg",
+  "./assets/dog1.webp",
+  "./assets/dog2.webp",
+  "./assets/dog3.avif",
+  "./assets/dog5.webp",
 ];
+let i = 0;
+let autoCount;
+const imageElement = document.getElementById("carousel-img");
+const backButton = document.getElementById("backward-btn");
+const foreButton = document.getElementById("forward-btn");
+const autoBackButton = document.getElementById("autoBackeard-btn");
+const autoForeButton = document.getElementById("autoForward-btn");
+const stopButton = document.getElementById("stop-btn");
+foreButton.addEventListener("click", () => {
+  nextImage();
+});
+function nextImage() {
+  i += 1;
+  if (i == images.length) {
+    //makes a rotation from the last image
 
+    i = 0;
+  }
+  imageElement.setAttribute("src", images[i]);
+}
+backButton.addEventListener("click", () => {
+  previousImage();
+});
+function previousImage() {
+  i -= 1;
+  if (i == -1) {
+    //makes a rotation from first image
+    i = images.length - 1;
+  }
 
-// Write your code here
+  imageElement.setAttribute("src", images[i]);
+}
+autoForeButton.addEventListener("click", () => {
+  autoCount = setInterval(nextImage, 2000);
+});
+stopButton.addEventListener("click", () => {
+  stopAuto();
+});
+function stopAuto() {
+  clearInterval(autoCount);
+}
+autoBackButton.addEventListener("click", () => {
+  autoCount = setInterval(previousImage, 2000);
+});
