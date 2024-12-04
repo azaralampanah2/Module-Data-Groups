4a0d06fa3e704be4090a31b6ca5f93e146036c77
azaralampanah2 <azaralampanah2@gmail.com>
2024-12-04 05:19:15 +0000

    complete Sprint-3/todo-list

--- Sprint-3/todo-list/script.js
+++ Sprint-3/todo-list/script.js
@@ -1,3 +1,11 @@
+const toDoInput = document.getElementById("inputToDo");
+const addToDoBtn = document.getElementById("addToDo");
+const container = document.getElementById("todo-list");
+
+// Event listener for button click
+addToDoBtn.addEventListener("click", () => {
+  addNewTodo(event);
+});
 function populateTodoList(todos) {
   let list = document.getElementById("todo-list");
   // Write your code to create todo list elements with completed and delete buttons here, all todos should display inside the "todo-list" element.
@@ -14,8 +22,23 @@ populateTodoList(todos);
 
 // This function will take the value of the input field and add it as a new todo to the bottom of the todo list. These new todos will need the completed and delete buttons adding like normal.
 function addNewTodo(event) {
-  // The code below prevents the page from refreshing when we click the 'Add Todo' button.
+  const textValue = toDoInput.value; // Get and trim the input value
+
+  // Create a new list item
+  const listItem = document.createElement("li");
+  const button1 = document.createElement("button");
+  button1.innerText = "complete";
+  // Set the text content of the list item
+  listItem.innerText = textValue;
+
+  // Append the list item to the list container
+  container.appendChild(listItem);
+  container.appendChild(button1);
+
+  toDoInput.value = "";
   event.preventDefault();
+  // The code below prevents the page from refreshing when we click the 'Add Todo' button.
+
   // Write your code here... and remember to reset the input field to be blank after creating a todo!
 }
 
