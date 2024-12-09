9634a77b5d20d471111b40d17ea25b691ab21310
azaralampanah2 <azaralampanah2@gmail.com>
2024-12-09 11:09:50 +0000

    amending Sprint-1/fix/ median.js and median.test.js to comply with reviewer comment

--- Sprint-1/fix/median.test.js
+++ Sprint-1/fix/median.test.js
@@ -8,19 +8,13 @@ const calculateMedian = require("./median.js");
 
 describe("calculateMedian", () => {
   test("returns the median for odd length array", () => {
-   // expect(calculateMedian([1, 2, 3])).toEqual(2);
+    expect(calculateMedian([1, 2, 3])).toEqual(2);
     expect(calculateMedian([1, 2, 3, 4, 5])).toEqual(3);
   });
 
   test("returns the average of middle values for even length array", () => {
     expect(calculateMedian([1, 2, 3, 4])).toEqual(2.5);
     expect(calculateMedian([1, 2, 3, 4, 5, 6])).toEqual(3.5);
-  });
-
-  test("doesn't modify the input", () => {
-    const list = [1, 2, 3];
-    calculateMedian(list);
-
-    expect(list).toEqual([1, 2, 3]);
+    expect(calculateMedian([1, 2, 3, 4, 100, 100])).toEqual(3.5);
   });
 });
