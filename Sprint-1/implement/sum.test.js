c1e591d6e63ad8d5a5dc3585582a1afbf84b15ba
Azar Alampanah <azaralampanah2@gmail.com>
2024-12-14 16:07:00 +0000

    Revert sum.test.js

--- Sprint-1/implement/sum.test.js
+++ Sprint-1/implement/sum.test.js
@@ -13,7 +13,7 @@ const sum = require("./sum.js");
 // Given an empty array
 // When passed to the sum function
 // Then it should return 0
-test.todo("given an empty array, returns 0");
+test.todo("given an empty array, returns 0")
 
 // Given an array with just one number
 // When passed to the sum function
