40c4e3d985ffd096d3d5b5cc30df1a2c24c0da16
Azar Alampanah <azaralampanah2@gmail.com>
2024-12-14 16:10:35 +0000

    Revert includes.js

--- Sprint-1/refactor/includes.js
+++ Sprint-1/refactor/includes.js
@@ -1,16 +1,13 @@
 // Refactor the implementation of includes to use a for...of loop
 
 function includes(list, target) {
-  // for (let index = 0; index < list.length; index++) {
-  // const element = list[index];
-  //if (element === target) {
-  for (let element of list) {
-    if (element === target) {
-      return true;
+  for (let index = 0; index < list.length; index++) {
+  const element = list[index];
+  if (element === target) {
+    return true;
     }
   }
-
   return false;
 }
-//console.log(includes([1, 4, 3, 5, 2], 3));
+
 module.exports = includes;
